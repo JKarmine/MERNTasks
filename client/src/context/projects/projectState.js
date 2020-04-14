dcdca6bf200257094882cd50f43b3ba46e1840b5
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-14 12:46:30 -0500

    projectContext, projectReducer, projectState and types

---
+++ client/src/context/projects/projectState.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+import projectContext from './projectContext';
+import projectReducer from './projectReducer';
+
+const ProjectState = (props) => {
+    const initialState = {
+        formulario: false
+    };
+
+    // Dispatch to execute actions
+    const [state, dispatch] = useReducer(projectReducer, initialState);
+
+    // CRUD functions
+
+    return (
+        <projectContext.Provider
+            value={{
+                formulario: state.formulario
+            }}
+        >
+            {props.children}
+        </projectContext.Provider>
+    )
+}
+ 
+export default ProjectState;
