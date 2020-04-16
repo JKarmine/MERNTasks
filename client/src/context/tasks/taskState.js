6b6880d4d12af5b09c99daebb4a66ff3c9574917
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-15 21:53:11 -0500

    taskState, taskContext and taskReducer

---
+++ client/src/context/tasks/taskState.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from 'react';
+import TaskContext from './taskContext';
+import TaskReducer from './taskReducer';
+
+const TaskState = (props) => {
+    const initialState = {
+        tareas: [],
+    };
+
+    // create dispatch
+
+    const [state, dispatch] = useReducer(TaskReducer, initialState);
+
+    return(
+        <TaskContext.Provider>
+            {props.children}
+        </TaskContext.Provider>
+    )
+}
+ 
+export default TaskState;
