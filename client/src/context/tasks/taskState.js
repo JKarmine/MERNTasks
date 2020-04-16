440a2c3aef72a431fcaad0bae3a8f8ff269cec1c
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-15 21:57:20 -0500

    tasks in state

--- client/src/context/tasks/taskState.js
+++ client/src/context/tasks/taskState.js
@@ -4,7 +4,21 @@ import TaskReducer from './taskReducer';
 
 const TaskState = (props) => {
     const initialState = {
-        tareas: [],
+        tasks: [
+            { name: 'Elegir plataforma', state: true, projectId: 1 },
+            { name: 'Elegir colores', state: false, projectId: 2 },
+            { name: 'Elegir formas de pago', state: true, projectId: 3 },
+            { name: 'Elegir hosting', state: false, projectId: 4 },
+            { name: 'Elegir plataforma', state: true, projectId: 1 },
+            { name: 'Elegir colores', state: false, projectId: 2 },
+            { name: 'Elegir formas de pago', state: true, projectId: 3 },
+            { name: 'Elegir plataforma', state: true, projectId: 4 },
+            { name: 'Elegir colores', state: false, projectId: 2 },
+            { name: 'Elegir formas de pago', state: true, projectId: 1 },
+            { name: 'Elegir plataforma', state: true, projectId: 3 },
+            { name: 'Elegir colores', state: false, projectId: 4 },
+            { name: 'Elegir formas de pago', state: true, projectId: 3 },
+        ]
     };
 
     // create dispatch
@@ -12,7 +26,11 @@ const TaskState = (props) => {
     const [state, dispatch] = useReducer(TaskReducer, initialState);
 
     return(
-        <TaskContext.Provider>
+        <TaskContext.Provider
+            value={{
+                tasks: state.tasks
+            }}
+        >
             {props.children}
         </TaskContext.Provider>
     )
