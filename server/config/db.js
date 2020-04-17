469ba7bf4152129ec48e1244c053859d818eba56
Jose Bustillo <josebustillodealba@gmail.com>
2020-04-16 22:27:48 -0500

    DB Connection

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB connected');
+    } catch (error) {
+        console.log(error);
+        process.exit(1); // Stop
+    }
+}
+
+module.exports = connectDB;
