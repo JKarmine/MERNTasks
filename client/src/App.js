b253f8901562cb32b7e682199bad53fa6ac0fede
jkarmine <josebustillodealba@gmail.com>
2020-04-24 22:40:08 -0500

    alertContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Projects from './components/projects/Projects';
+
 import ProjectState from './context/projects/projectState';
 import TaskState from './context/tasks/taskState';
+import AlertState from './context/alerts/alertState';
 
 function App() {
   return (
     <ProjectState>
       <TaskState>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/projects" component={Projects} />
-          </Switch>
-        </Router>
+        <AlertState>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/projects" component={Projects} />
+            </Switch>
+          </Router>
+        </AlertState>
       </TaskState>
     </ProjectState>
   );
