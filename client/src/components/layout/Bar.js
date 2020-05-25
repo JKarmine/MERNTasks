6e06a31474698dade3e83f4eb8240b76300d3227
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-05-24 20:29:40 -0500

    feat: name in header

--- client/src/components/layout/Bar.js
+++ client/src/components/layout/Bar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AuthContext from '..//../context/auth/authContext';
 
 const Bar = () => {
+
+    // Get auth info
+    const authContext = useContext(AuthContext);
+    const { user, userAuth } = authContext;
+
+    useEffect(() => {
+        userAuth();
+    }, []);
+
     return (
         <header className="app-header">
-            <p className="nombre-usuario">Hello, <span>Jose</span></p>
+            { user && <p className="nombre-usuario">Hello, <span>{user.name}</span></p>}
 
             <nav className="nav-principal">
                 <a href="#!">Log out</a>
