ff8f7dab2e1771b3183c4a9cbfe39ba6db74d474
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-05-24 20:22:18 -0500

    feat: check if there's a token

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ import ProjectState from './context/projects/projectState';
 import TaskState from './context/tasks/taskState';
 import AlertState from './context/alerts/alertState';
 import AuthState from './context/auth/authState';
+import tokenAuth from './config/tokenAuth';
+
+// Check if there's a token
+const token = localStorage.getItem('token');
+if (token) {
+  tokenAuth(token);
+}
 
 function App() {
   return (
