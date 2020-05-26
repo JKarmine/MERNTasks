24912128d044fd0a5e3dc66677a988f7d7826aa4
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-05-26 11:40:27 -0500

    feat: alertContext and showAlert

--- client/src/components/projects/List.js
+++ client/src/components/projects/List.js
@@ -1,23 +1,33 @@
 import React, { useContext, useEffect } from 'react';
 import Project from './Project';
 import projectContext from '../../context/projects/projectContext';
+import AlertContext from '../../context/alerts/alertContext';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const List = () => {
     // Projects from state
     const projectsContext = useContext(projectContext);
-    const { projects, getProjects } = projectsContext;
+    const { message, projects, getProjects } = projectsContext;
+
+    const alertContext = useContext(AlertContext);
+    const { alert, showAlert } = alertContext;
 
     useEffect(() => {
+        // If error
+        if (message) {
+            showAlert(message.msg, message.category);
+        }
+
         getProjects();
         //eslint-disable-next-line
-    }, []);
+    }, [message]);
 
     // Validation
     if (projects.length === 0) return <p>No projects. Start creating one.</p>;
 
     return (
         <ul className="listado-proyectos">
+            { alert && <div className={`alerta ${alert.category}`}>{alert.msg}</div> }
             <TransitionGroup>
                 {projects.map(project => (
                     <CSSTransition
